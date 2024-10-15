1ac4e6745ea16e3139e75b9941b899527ef1852c
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2024-10-15 20:49:49 +0000

    chapter completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess between 1 and 100:");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // rust allows shadowing variables, so we can reuse guess for the parsed value
+        let guess: u32 = guess.trim().parse().expect("Not a valid number");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small! Try again."),
+            Ordering::Greater => println!("Too big! Try again."),
+            Ordering::Equal => {
+                println!("You won! The secret number was {}", secret_number);
+                break;
+            }
+        }
+    }
+
+    // if guessed_number == secret_number  {
+    //     println!("you won! The secret number was {}", secret_number);
+    // }
+    // else {
+    //     println!("you failed! The secret number was {}", secret_number);
+    // }
 }
