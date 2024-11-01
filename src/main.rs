1887d4bcbb88982441b5e662c6d2cdea6ee9c46b
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2024-11-01 17:42:06 +0000

    string slices

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
-
 fn main() {
     let s = String::from("Hello, world!");
 
-    let (s2, len) = take_ownership(s);
+    let first_word = first_word(&s);
 
-    println!("{} ownership has been returned. Len {}", s2, len);
+    println!("The first word of {} is: {}", s, first_word);
 
-    let i: i32 = 42;
+    // let (s2, len) = take_ownership(s);
 
-    makes_copy(i);
+    // println!("{} ownership has been returned. Len {}", s2, len);
 
-    println!("{}", i);
+    // let i: i32 = 42;
+
+    // makes_copy(i);
+
+    // println!("{}", i);
 }
 
-fn take_ownership(s: String) -> (String, usize) {
-    println!("{} has been moved", s);
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
 
-    let len = s.len();
-    
-    (s, len)
+    for (i, &byte) in bytes.iter().enumerate() {
+        if byte == b' ' || byte == b'\t' || byte == b'\n' || byte == b',' {
+            return &s[0..i];
+        }
+    }
+    return &s[..];
 }
 
-fn makes_copy(i: i32) {
-    println!("{} has been copied", i);
-}
+// fn take_ownership(s: String) -> (String, usize) {
+//     println!("{} has been moved", s);
 
+//     let len = s.len();
+
+//     (s, len)
+// }
+
+// fn makes_copy(i: i32) {
+//     println!("{} has been copied", i);
+// }
